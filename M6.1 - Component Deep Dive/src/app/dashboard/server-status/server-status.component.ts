071c0decbca2892fb48f9985f6eb8be26a8253a6
Nikil Saini <nikil.saini@docquity.com>
2025-05-03 14:38:45 +0530

    destroy ref

--- M6.1 - Component Deep Dive/src/app/dashboard/server-status/server-status.component.ts
+++ M6.1 - Component Deep Dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { timeInterval } from 'rxjs';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { interval, timeInterval } from 'rxjs';
 
 @Component({
   selector: 'app-server-status',
@@ -9,9 +9,15 @@ import { timeInterval } from 'rxjs';
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent {
-  currentStatus : 'online' | 'offline' | 'unknown' = 'online';
-  constructor() {
-    setInterval(() => { 
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  private destroyRef = inject(DestroyRef);
+
+  private intereval?: ReturnType<typeof setInterval>;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.intereval = setInterval(() => { 
       const rnd = Math.random();
       if(rnd<0.4){
         this.currentStatus = 'online';
@@ -23,5 +29,14 @@ export class ServerStatusComponent {
         this.currentStatus = 'unknown';
       }
     }, 2000)
+
+    // Alternative to ngOnDestroy
+    this.destroyRef.onDestroy(()=>{
+      // clearInterval(interval); declare interval of type const and not ReturnType.. for this usage
+    })
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intereval)
   }
 }
